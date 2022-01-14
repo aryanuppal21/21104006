ac54b9054fc4128f80755df1da9da2471d201a10
aryanuppal21 <97751462+aryanuppal21@users.noreply.github.com>
2022-01-14 22:27:56 +0530

    Add files via upload

---
+++ assignmentoutput_21104006.py
@@ -0,0 +1,29 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\SHREYA JINDAL\AppData\Local\Programs\Python\Python310\assignment1inputs_21104006.py
+first question
+enter first number:-65.4
+enter second number:-76.7
+enter third number:-45.3
+average of the three numbers is :- 62.466666666666676
+second question
+enter your gross income in dollars:65000
+enter the number of dependants4
+your total tax amount is: 860.0
+third question
+student-[SID,NAME,GENDER,DEPT NAME,CGPA]
+student= [6, 'aryan', 'M', 'ELE', 9.3]
+fourth question
+marks of five students
+enter the marks of the first student:34.6
+enter the marks of the second student:76.9
+enter the marks of the third student:98.5
+enter the marks of the fourth student:50.7
+enter the marks of the fifth student:60.7
+marks before sorting: [34.6, 76.9, 98.5, 50.7, 60.7]
+marks after sorting: [34.6, 50.7, 60.7, 76.9, 98.5]
+fifth question A part
+['red', 'green', 'white', 'pink', 'yellow']
+fifth qyestion b part
+['red', 'green', 'white', 'purple', 'yellow']
