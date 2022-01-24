b8807a09c07d3bd037ce54b98d55012babf7b47c
aryanuppal21 <97751462+aryanuppal21@users.noreply.github.com>
2022-01-24 17:08:22 +0530

    Add files via upload

---
+++ aryan uppal  outputs assignment 2.py
@@ -0,0 +1,43 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\SHREYA JINDAL\AppData\Local\Programs\Python\Python310\aryan inputs assignment 2.py
+(a)
+Give the statement:python is case sensitive
+python is case sensitive
+24
+(b)
+evitisnes esac si nohtyp
+(c)
+case sensitive
+(d)
+python is object oriented
+(e)
+11
+(f)
+pythoniscasesensitive
+Hey,aryan uppal Here!
+My SID is 21104006
+I am from ElE department and my CGPA is 9.9
+(a)
+0b1000
+(b)
+0b111010
+(c)
+0b110010
+(d)
+0b11100000
+0b101000
+(e)
+0b1110
+0b0
+Give a number:34
+Give a number:67
+Give a number:12
+67 is greatest number
+Give a word:namerun
+Yes
+Give a number:3
+Give a number:4
+Give a number:5
+The triangle is possible
