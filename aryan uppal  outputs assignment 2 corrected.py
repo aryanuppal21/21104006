e66fc26783280f287c91ca8b027b4ea04e725367
aryanuppal21 <97751462+aryanuppal21@users.noreply.github.com>
2022-01-25 16:47:45 +0530

    Add files via upload

---
+++ aryan uppal  outputs assignment 2 corrected.py
@@ -0,0 +1,43 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\SHREYA JINDAL\AppData\Local\Programs\Python\Python310\aryan inputs assignment 2.py
+(a)
+Give the statement:python is a case sesitive language
+python is a case sesitive language
+34
+(b)
+egaugnal evitises esac a si nohtyp
+(c)
+a case sesitive 
+(d)
+python is object orientedlanguage
+(e)
+10
+(f)
+pythonisacasesesitivelanguage
+Hey,aryan uppal Here!
+My SID is 21104006
+I am from ElE department and my CGPA is 9.900000
+(a)
+0b1000
+(b)
+0b111010
+(c)
+0b110010
+(d)
+0b11100000
+0b101000
+(e)
+0b1110
+0b0
+Give a number:34
+Give a number:83
+Give a number:18
+83 is greatest number
+Give a word:namesake
+Yes
+Give a number:5
+Give a number:6
+Give a number:7
+The triangle is possible
