bc4820279bf291f59e55307764ac31d10fdbaca4
aryanuppal21 <97751462+aryanuppal21@users.noreply.github.com>
2022-02-14 23:36:34 +0530

    Add files via upload

---
+++ 21104006_ass3output.py
@@ -0,0 +1,96 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\SHREYA JINDAL\AppData\Local\Programs\Python\Python310\21104006_ass3input.py
+The solution of Question 1 is:
+
+Enter the string: aryan told aryan that his name was aryan
+The occurence(s) of:
+	"aryan" is/are 3
+	"told" is/are 1
+	"that" is/are 1
+	"his" is/are 1
+	"name" is/are 1
+	"was" is/are 1
+
+The solution of Question 2 is:
+
+Day - 29
+Month - 2
+Year - 2025
+The given month has only 28 days
+Please enter a valid date
+Day - 29
+Month - 2
+Year - 2004
+Next Date is: 1/3/2004
+
+The solution of Question 3 is:
+
+Enter the list: [1,2,3,4,5,6]
+Output: [(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36)]
+
+The solution of Question 4 is:
+
+Enter the grade point of the student: 9.7
+Your Grade is 'A' and Excellent Performance
+
+The solution of Question 5 is:
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+The solution of Question 6 is:
+
+Enter the name of the student: aryan uppal 
+Enter the SID of aryan uppal : 21104006
+
+You have entered 1 value(s) till now
+Do you want to enter more data? y
+Enter the name of the student: pratyush soni 
+Enter the SID of pratyush soni : 21104067
+
+You have entered 2 value(s) till now
+Do you want to enter more data? y
+Enter the name of the student: prabhnoor singh
+Enter the SID of prabhnoor singh: 21104063
+
+You have entered 3 value(s) till now
+Do you want to enter more data? n
+
+a. Student Details:
+The SID of aryan uppal  is 21104006
+The SID of pratyush soni  is 21104067
+The SID of prabhnoor singh is 21104063
+
+b. Student Details (sorted with respect to names):
+The SID of aryan uppal  is 21104006
+The SID of prabhnoor singh is 21104063
+The SID of pratyush soni  is 21104067
+
+c. Student Details (sorted with respect to SIDs):
+The SID of aryan uppal  is 21104006
+The SID of prabhnoor singh is 21104063
+The SID of pratyush soni  is 21104067
+
+d. Enter the SID of the student: 21104006
+The name of student whose SID is 21104006 is aryan uppal 
+
+The solution of Question 7 is:
+
+Enter the no. of terms of the Fibonacci sequence: 9
+The Fibonacci sequence is as follows:
+0 1 1 2 3 5 8 13 21 
+The average of the terms of Fibonacci sequence upto 9 terms is 6.00
+
+The solution of Question 8 is:
+
+a. Set of all elements that are in Set1 and Set2 but not both: {1, 3, 5, 6, 8}
+b. Set of all elements that are in only one of the three sets Set1, Set2 and Set3: {17, 3, 6, 8, 9, 13}
+c. Set of elements that are in exactly two of the sets Set1, Set2 and Set3: {1, 2, 4, 5}
+d. Set of all integers in the range 1 to 10 that are not in Set1: {6, 7, 8, 9, 10}
+e. Set of all integers in the range 1 to 10 that are not in Set1, Set2 and Set3: {10, 7}
