1feaeca7af4b4dc505fbd6ea90ad1e5ba18a9956
aryanuppal21 <97751462+aryanuppal21@users.noreply.github.com>
2022-03-01 22:35:59 +0530

    Add files via upload

---
+++ ass4_output_21104006.py
@@ -0,0 +1,73 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\SHREYA JINDAL\AppData\Local\Programs\Python\Python310\ass4_input_21104006.py.py
+Move disk 1 from source A to destination B.
+Move disk 2 from source A to destination C.
+Move disk 1 from source B to destination C.
+Move disk 3 from source A to destination B.
+Move disk 1 from source C to destination A.
+Move disk 2 from source C to destination B.
+Move disk 1 from source A to destination B.
+Enter the size of pascals triangle 4
+USING FOR LOOP
+     1 
+    1 1 
+   1 2 1 
+  1 3 3 1 
+
+
+
+USING WHILE LOOP
+     1 
+    1 1 
+   1 2 1 
+  1 3 3 1 
+
+
+
+USING RECURSSIONS
+      1   
+    1   1   
+  1   2   1   
+1   3   3   1   
+
+
+Enter first integer value(dividend): 567
+Enter second integer value(divisor): 45
+
+Quotient: 12 
+Remainder: 27
+
+a. Check whether the function (divmod()) is callable or not:
+True, which means it is callable
+
+b. Check whether all the values are zero or not:
+All values in result(i.e. quotient and remainder) are non-zero.
+
+c. Add (4,5,6) to the result and filter out values greater than 4:
+Values greater than 4(in list format) are: [5, 6, 12, 27]
+
+d. Convert the above result into a set datatype:
+The output of previous part in set datatype would be: {27, 12, 5, 6}
+
+e. Make the set immutable:
+The immutable set would be: frozenset({27, 12, 5, 6})
+
+f. Evaluate the maximum value from the set and find out its hash value:
+The maximum value from the set is: 27
+The hash value of 27(considering it to be integer) is 27 and its hash value is -3162869481920262287(if we consider 27 as a string).
+Enter name of student: aryan uppal
+Enter SID of aryan uppal: 21104006
+Object Created
+
+The name of the student is aryan uppal and his/her roll number is 21104006
+
+Object destroyed
+a. The updated salary of Mehak is 70000 
+b. Record of Viren deleted
+
+Enter the word: top
+Enter a new meaningful word using the exact same letters to test your friendship: pot
+Does the word makes sense?(y/Y or n/N ) y
+The friends pass the friendship test
